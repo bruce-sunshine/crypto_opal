922be82a0ec4ef19ebd8afc8d945a30f45bb993c
bruce <lixiaolongmessi@163.com>
2018-08-14 17:17:19 +0800

    ptlib

---
+++ ptlib-2.10.11_arm/plugins/sound_alsa/alsa_bak_cxx_h/3hdmifail_sound_alsa.h
@@ -0,0 +1,87 @@
+#include <ptlib.h>
+#include <ptlib/sound.h>
+
+#define ALSA_PCM_NEW_HW_PARAMS_API 1
+#include <alsa/asoundlib.h>
+
+
+class PSoundChannelALSA : public PSoundChannel {
+ public:
+  PSoundChannelALSA();
+  void Construct();
+  PSoundChannelALSA(
+    const PString &device,
+    PSoundChannel::Directions dir,
+    unsigned numChannels,
+    unsigned sampleRate,
+    unsigned bitsPerSample
+  );
+  ~PSoundChannelALSA();
+  static PStringArray GetDeviceNames(PSoundChannel::Directions);
+  static PString GetDefaultDevice(PSoundChannel::Directions);
+  PBoolean Open(
+    const PString & device,
+    Directions dir,
+    unsigned numChannels,
+    unsigned sampleRate,
+    unsigned bitsPerSample
+  );
+  PBoolean Setup();
+  PBoolean Close();
+  PBoolean Write(const void * buf, PINDEX len);
+  PBoolean Read(void * buf, PINDEX len);
+  PBoolean SetFormat(unsigned numChannels, unsigned sampleRate, unsigned bitsPerSample);
+  unsigned GetChannels() const;
+  unsigned GetSampleRate() const;
+  unsigned GetSampleSize() const;
+  PBoolean SetBuffers(PINDEX size, PINDEX count);
+  PBoolean GetBuffers(PINDEX & size, PINDEX & count);
+  PBoolean PlaySound(const PSound & sound, PBoolean wait);
+  PBoolean PlayFile(const PFilePath & filename, PBoolean wait);
+  PBoolean HasPlayCompleted();
+  PBoolean WaitForPlayCompletion();
+  PBoolean RecordSound(PSound & sound);
+  PBoolean RecordFile(const PFilePath & filename);
+  PBoolean StartRecording();
+  PBoolean IsRecordBufferFull();
+  PBoolean AreAllRecordBuffersFull();
+  PBoolean WaitForRecordBufferFull();
+  PBoolean WaitForAllRecordBuffersFull();
+  PBoolean Abort();
+  PBoolean SetVolume(unsigned);
+  PBoolean GetVolume(unsigned &);
+  PBoolean IsOpen() const;
+
+ private:
+  static void UpdateDictionary(PSoundChannel::Directions);
+  bool SetHardwareParams();
+  bool SetHardwareParams_t(snd_pcm_t *handle);
+  PBoolean Volume(PBoolean, unsigned, unsigned &);
+  PBoolean writetohw(snd_pcm_t *handle,const void * buf, PINDEX len, PBoolean enablelastcount);
+
+  PSoundChannel::Directions direction;
+  PString device;
+  PString device1;
+  PString device_hdmi;
+  int inputdev;
+  FILE *pf;
+  unsigned mNumChannels;
+  unsigned mSampleRate;
+  unsigned mBitsPerSample;
+  PBoolean isInitialised;
+
+  snd_pcm_t *os_handle; /* Handle, different from the PChannel handle */
+  int card_nr;
+  snd_pcm_t *os_handle1; /* Handle, different from the PChannel handle */
+  int card_nr1;
+  snd_pcm_t *os_handle_hdmi; /* Handle, different from the PChannel handle */
+  int card_nr_hdmi;
+
+  PMutex device_mutex;
+
+  PINDEX m_bufferSize;
+  PINDEX m_bufferCount;
+
+  /** Number of bytes in a ALSA frame. a frame may only be 4ms long*/
+  int frameBytes; 
+};
